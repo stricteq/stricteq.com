0c6435054ad8a1bbedd0b11b2f2c4bc54d97adc2
Kyle E. Mitchell <kyle@kemitchell.com>
2020-07-08 19:46:20 -0700

    Test payment with a 3D Secure card

--- test/payment.test.js
+++ test/payment.test.js
@@ -1,5 +1,6 @@
 const createProject = require('./create-project')
 const login = require('./login')
+const logout = require('./logout')
 const server = require('./server')
 const signup = require('./signup')
 const tape = require('tape')
@@ -54,6 +55,7 @@ tape('declined cards', test => {
     const disconnectText = await disconnect.getText()
     test.equal(disconnectText, 'Disconnect Stripe Account', 'connected')
     await createProject({ browser, port, project, url, price, category })
+    await logout({ browser, port })
     // Buy licenses.
     for await (const number of Object.keys(testNumbers)) {
       const groups = number.match(/.{2}/g)
@@ -92,6 +94,73 @@ tape('declined cards', test => {
       const watchWord = testNumbers[number]
       test.assert(errorText.includes(watchWord), `declined: ${watchWord}`)
     }
+    test.end()
+    done()
+  }, 8080)
+})
+
+tape('3D Secure card', test => {
+  const customerName = 'Jon Doe'
+  const customerEMail = 'jon@exaple.com'
+  const customerLocation = 'US-CA'
+  server(async (port, done) => {
+    const browser = await webdriver()
+    await new Promise((resolve, reject) => signup({
+      browser, port, name, location, handle, password, email
+    }, error => {
+      if (error) reject(error)
+      resolve()
+    }))
+    await login({ browser, port, handle, password })
+    // Connect.
+    const account = await browser.$('#account')
+    await account.click()
+    const connect = await browser.$('#connect')
+    await connect.click()
+    const skip = await browser.$('=Skip this account form')
+    await skip.click()
+    // Confirm connected.
+    const disconnect = await browser.$('#disconnect')
+    const disconnectText = await disconnect.getText()
+    test.equal(disconnectText, 'Disconnect Stripe Account', 'connected')
+    await createProject({ browser, port, project, url, price, category })
+    await logout({ browser, port })
+    // Buy licenses.
+    const number = '4000000000003220'
+    const groups = number.match(/.{2}/g)
+    await browser.navigateTo(`http://localhost:${port}/~${handle}/${project}`)
+    // Fill in customer details.
+    const nameInput = await browser.$('#buyForm input[name=name]')
+    await nameInput.addValue(customerName)
+    const emailInput = await browser.$('#buyForm input[name=email]')
+    await emailInput.addValue(customerEMail)
+    const locationInput = await browser.$('#buyForm input[name=location]')
+    await locationInput.addValue(customerLocation)
+    // Enter credit card information.
+    const iframe = await browser.$('iframe')
+    await browser.switchToFrame(iframe)
+    const cardNumber = await browser.$('input[name="cardnumber"]')
+    for await (const group of groups) {
+      await cardNumber.addValue(group)
+      await timeout(200)
+    }
+    const expiration = await browser.$('input[name="exp-date"]')
+    await expiration.setValue('10 / 31')
+    const cvc = await browser.$('input[name="cvc"]')
+    await cvc.setValue('123')
+    const postal = await browser.$('input[name="postal"]')
+    await postal.setValue('12345')
+    await browser.switchToParentFrame()
+    // Accept terms.
+    const terms = await browser.$('#buyForm input[name=terms]')
+    await terms.click()
+    // Click the buy button.
+    const submit = await browser.$('#buyForm button[type=submit]')
+    await submit.click()
+    const message = await browser.$('.message')
+    await message.waitForExist()
+    const messageText = await message.getText()
+    test.assert(messageText.includes('Thank you', 'confirmation'))
     test.end()
     done()
   }, 8080)
