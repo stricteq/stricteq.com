dc40aa25395871fe3b1dca16a0cb97d6869a2c21
Kyle E. Mitchell <kyle@kemitchell.com>
2020-08-21 08:26:44 -0700

    Add Deal link to <footer>

--- index.js
+++ index.js
@@ -284,6 +284,7 @@ const header = `
 
 const footer = `
 <footer role=contentinfo>
+  <a class=spaced href=/deal>Deal</a>
   <a class=spaced href=https://artlessdevices.com>Company</a>
   <a class=spaced href=/service>Pricing</a>
   <a class=spaced href=/service>Terms of Service</a>
