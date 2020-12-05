d288047f4c94ca93b7fbe8f5d8e19d4cc2489aaf
Kyle E. Mitchell <kyle@kemitchell.com>
2020-12-04 22:24:37 -0800

    commonmark@0.29.3

--- markdown.js
+++ markdown.js
@@ -1,11 +1,11 @@
 // Helper Function for Rendering Markdown
 
-import commonmark from 'commonmark'
+import { Parser, HtmlRenderer } from 'commonmark'
 
 export default (markup, options) => {
   const { safe = false } = (options || {})
-  const reader = new commonmark.Parser({ smart: true })
-  const writer = new commonmark.HtmlRenderer({ safe })
+  const reader = new Parser({ smart: true })
+  const writer = new HtmlRenderer({ safe })
   const parsed = reader.parse(markup)
   return writer.render(parsed)
 }
