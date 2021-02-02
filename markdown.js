7c8f9e7cf681e365a4e99bf87637eeb09f396abb
Kyle E. Mitchell <kyle@kemitchell.com>
2021-02-01 21:04:52 -0800

    Use Remarkable to render Markdown

--- markdown.js
+++ markdown.js
@@ -1,11 +1,24 @@
 // Helper Function for Rendering Markdown
 
-import { Parser, HtmlRenderer } from 'commonmark'
+import { Remarkable } from 'remarkable'
 
 export default (markup, options) => {
   const { safe = false } = (options || {})
-  const reader = new Parser({ smart: true })
-  const writer = new HtmlRenderer({ safe })
-  const parsed = reader.parse(markup)
-  return writer.render(parsed)
+  const parser = new Remarkable({
+    html: !safe,
+    typographer: true
+  }).use(headerIDs)
+  return parser.render(markup)
+}
+
+function headerIDs (remarkable) {
+  remarkable.renderer.rules.heading_open = (tokens, index) => {
+    const level = tokens[index].hLevel
+    const text = tokens[index + 1].content
+    const slug = text
+      .toLowerCase()
+      .replace(/ /g, '-')
+      .replace(/[^a-z0-9-]/g, '')
+    return `<h${level} id="${slug}">`
+  }
 }
